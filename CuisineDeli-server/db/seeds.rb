1ca3c3636974fd8c1cb5a884bea212cbbdfafd92
Christopherchinia <chinia214@gmail.com>
2022-09-05 10:53:23 +0300

    populating the seed file with data and also creating the comment file in the model in the cuisineDeli folder

--- CuisineDeli-server/db/seeds.rb
+++ CuisineDeli-server/db/seeds.rb
@@ -3,25 +3,125 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 Comment.create([
     {
-      body: "Hello 👋",
-      username: "Liza"
+      food:"Hot Pizza",
+      feedback: "I just want to get alittle bit of this, alittle bit of that and alittle more.",
+      author: "Troy"
     },
     {
-      body: "Hi!",
-      username: "Duane"
+      food:"Spicy chicken",
+      feedback: "So affordable and sweet.",
+      author: "Duane"
     },
     {
-      body: "let's get this chat app working",
-      username: "Liza"
+      food:"Spicy Noodles",
+      feedback: "Wow just wow!",
+      author: "Liza"
     },
     {
-      body: "ngl, this looks like a lot 😬",
-      username: "Duane"
+      food:"Burger",
+      feedback: "I havent tasted anything like this.",
+      author: "Denzel"
     },
     {
-      body: "You got this! 💪",
-      username: "Liza"
-    }
+      food:"Fries",
+      feedback: "Am about to buy the whole stock",
+      author: "Chris"
+    },
+    {
+        food:"Hot Pizza",
+        feedback: "Am speechless bruh...",
+        author: "Martin"
+      },
+      {
+        food:"Spicy chicken",
+        feedback: "Favourite dish",
+        author: "Joe"
+      },
+      {
+        food:"Spicy Noodles",
+        feedback: "Cooking like this is pure gold.",
+        author: "Lil champ"
+      },
+      {
+        food:"Burger",
+        feedback: "I think am falling in love.",
+        author: "Kim"
+      },
+      {
+        food:"Fries",
+        feedback: "We be camping here all day",
+        author: "Tiana"
+      },
+      {
+        food:"Hot Pizza",
+        feedback: "My friends loved it",
+        author: "Tessa"
+      },
+      {
+        food:"Spicy chicken",
+        feedback: "So affordable",
+        author: "Brian"
+      },
+      {
+        food:"Spicy Noodles",
+        feedback: "Favourite dish",
+        author: "Andrew"
+      },
+      {
+        food:"Burger",
+        feedback: "I need the ingredients",
+        author: "Waridi"
+      },
+      {
+        food:"Fries",
+        feedback: "I need to stop eating or i'll go broke.",
+        author: "Priscillah"
+      },
+      {
+        food:"Hot Pizza",
+        feedback: "With cooking like this....",
+        author: "Lizzo"
+      },
+      {
+        food:"Spicy chicken",
+        feedback: "Sheeesh!",
+        author: "Nicki"
+      },
+      {
+        food:"Spicy Noodles",
+        feedback: "Where have you been all this time.",
+        author: "Ted"
+      },
+      {
+        food:"Burger",
+        feedback: "This one was small need a big one next time.",
+        author: "Jarvis"
+      },
+      {
+        food:"Capcakes",
+        feedback: "I need more of this.",
+        author: "Mayoo"
+      },
+      {
+        food:"Fries",
+        feedback: "I'll be back for more.",
+        author: "the Sauce"
+      },
+      {
+        food:"Pancakes",
+        feedback: "wouldnt miss this for the world",
+        author: "kidnextdoor"
+      },
+      {
+        food:"Beef",
+        feedback: "Wasnt cooked properly. so dissapointed",
+        author: "Maryanne"
+      },
+      {
+        food:"Fries",
+        feedback: "Tasty",
+        author: "Dee"
+      }
   ])
 
 puts "✅ Done seeding!"
